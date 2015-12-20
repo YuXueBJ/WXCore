1914d076c615695f2a4bf3d461742fdc4c234639
朱洪伟 <zhuhw@xiaoshouyi.com>
2015-12-20 17:05:53 +0800

    修改工程

--- WXPersistence/WXPersistence/NSObjectAddition.h
+++ WXPersistence/WXPersistence/NSObjectAddition.h
@@ -3,7 +3,7 @@
 //  WXPersistence
 //
 //  Created by 朱洪伟 on 15/11/12.
-//  Copyright © 2015年 朱洪伟. All rights reserved.
+//  Copyright © 2015年 www.zhwios.com. All rights reserved.
 //
 
 
--- WXPersistence/WXPersistence/NSObjectAddition.m
+++ WXPersistence/WXPersistence/NSObjectAddition.m
@@ -3,7 +3,7 @@
 //  WXPersistence
 //
 //  Created by 朱洪伟 on 15/11/12.
-//  Copyright © 2015年 朱洪伟. All rights reserved.
+//  Copyright © 2015年 个人博客：www.zhwios.com All rights reserved.
 //
 
 #import "NSObjectAddition.h"
